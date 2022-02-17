19446b9df0b054d938c6317eeee59714530eac37
Daniel Allen Gutierrez <icanmakespaghetti@gmail.com>
2022-02-17 15:31:49 -0800

    no uri encoding?

--- api/example.js
+++ api/example.js
@@ -29,7 +29,7 @@ exports.handler = async (event,context) =>
         let response = 
         {
             statusCode: 200,
-            body: encodeURI(JSON.stringify(sheetmetadata))
+            body: JSON.stringify(sheetmetadata)
         };
         return response;
     }
