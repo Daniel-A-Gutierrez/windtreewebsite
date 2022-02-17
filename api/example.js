d5a5f9b2f2098c6ac8a0ded0252b8081f8a63f70
Daniel Allen Gutierrez <icanmakespaghetti@gmail.com>
2022-02-17 15:06:36 -0800

    different library, doing tutorial

---
+++ api/example.js
@@ -0,0 +1,43 @@
+const {google} = require("googleapis");
+const { auth } = require("googleapis/node_modules/google-auth-library");
+
+exports.handler = async (event,context) => 
+{
+    try
+    {
+        const GOOGLE_SPREADSHEET_ID = process.env.ENV_CONTACT_SHEET_ID;
+
+        const serviceAccountAuth = new google.auth.JWT({
+            email: process.env.ENV_GOOGLE_SERVICE_ACCOUNT_EMAIL,
+            key: process.env.ENV_GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+            scopes: 'https://www.googleapis.com/auth/spreadsheets'
+          });
+
+        const client = await serviceAccountAuth.getClient();
+        const googleSheets = google.sheets(
+        {
+            version:"v4",
+            auth:"client"
+        });
+        const sheetmetadata = await googleSheets.spreadsheets.get(
+        {
+            auth:serviceAccountAuth,
+            spreadsheetId : GOOGLE_SPREADSHEET_ID
+        })
+          
+        let response = 
+        {
+            statusCode: 200,
+            body: sheetmetadata
+        };
+        return response;
+    }
+    catch(err)
+    {
+        let response = 
+        {
+            statusCode: 500,
+            body: err
+        };
+    }
+}
