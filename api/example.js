000b0f917452152d11a00c74e1c692800bd3c05b
Daniel Allen Gutierrez <icanmakespaghetti@gmail.com>
2022-02-17 15:30:15 -0800

    json must be capitalized

--- api/example.js
+++ api/example.js
@@ -29,7 +29,7 @@ exports.handler = async (event,context) =>
         let response = 
         {
             statusCode: 200,
-            body: encodeURI(json.stringify(sheetmetadata))
+            body: encodeURI(JSON.stringify(sheetmetadata))
         };
         return response;
     }
