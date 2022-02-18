9b7e4c69fc7f74e711f1e36a40a60517d429fff3
Daniel Allen Gutierrez <icanmakespaghetti@gmail.com>
2022-02-18 09:58:43 -0800

    no need to send rows

--- api/register.js
+++ api/register.js
@@ -75,7 +75,7 @@ exports.handler = async (event,context) =>
         let response = 
         {
             statusCode: 200,
-            body: JSON.stringify(getRows.data)
+            body: "success"
         };
         return response;
     }
