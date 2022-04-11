115849bc129b44d33b9854210012c3349e93bc94
Daniel Allen Gutierrez <icanmakespaghetti@gmail.com>
2022-04-11 16:07:34 -0700

    sending a whole page?

--- api/register.js
+++ api/register.js
@@ -85,8 +85,21 @@ exports.handler = async (event,context) =>
         await schools;
         let response = 
         {
-            statusCode: 301,
-            body: "/home"
+            statusCode: 200,
+            headers : {'Content-Type' : 'text/html'},
+            body: `<!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <meta http-equiv="Refresh" content="0; url='https://www.w3docs.com'" />
+                <title>Redirecting</title>
+            </head>
+            <body>
+                
+            </body>
+            </html>`
         };
         return response;
     }
